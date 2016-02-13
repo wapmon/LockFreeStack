9274d57f8395867ed538514e8ea11bc94f7a47c3
Austin Pearigen <wapmon@knights.ucf.edu>
2016-02-13 15:02:24 -0500

    Program is now running sequentially. Each thread gets 500,000 operations. Commented everything out. Need to start running with different parameters such as number of threads and distribution of stack operations. included how long each thread takes to complete.

--- src/lockFreeStack/Main.java
+++ src/lockFreeStack/Main.java
@@ -1,85 +1,135 @@
+//William Pearigen
+//Team 21 - Scalable Lock Free Stack Algorithm - Sequential Implementation
+//15 February, 2016
+//COP 4520 Parallel Processes 
+
 package lockFreeStack;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 	
+
+	//////////////////////////////////////////////////////////////
+	//////////////////// CLASS DECLARATIONS //////////////////////
+	//////////////////////////////////////////////////////////////
+
+	
+	/**
+	 * Cells make up the stack, with the "next" cell being the one
+	 * under the current cell, so the bottom cell in the stack will
+	 * have a null next cell
+	 */
 	public static class Cell{
 		Cell next;
 		char data;
-		
+
 		public Cell(Cell next, char data){
 			this.next = next;
 			this.data = data;
 		}
 	}
-	
+
+	/**
+	 * Shared stack object. Very simple, consists only of the top cell,
+	 * which points down the stack, similar to a linked list. 
+	 */
 	public static class SimpleStack{
 		Cell top;
-		
+
 		public SimpleStack(Cell top){
 			this.top = top;
 		}
 	}
-	
+
+	/** This class is not used in the sequential implementation,
+	  	and will be set to null for now **/
 	public static class AdaptParams{
 		int count;
 		double factor;
-		
+
 		public AdaptParams(int count, float factor){
 			this.count = count;
 			this.factor = factor;
 		}
 	}
-	
+
+	/**
+	 * This class extends Thread class in order to add an id, stack operation, current cell to use
+	 * and adapt parameters (not used in sequential implementation). The run method will create
+	 * an array of instructions for each thread, then go through each instruction on the stack until the
+	 * entire array has been performed
+	 */
 	public static class ThreadInfo extends Thread{
 		int id;
 		char op;
 		Cell cell;
 		AdaptParams adaptParams;
-		
+
 		public ThreadInfo(int id, char op, Cell cell, AdaptParams adaptParams){
 			this.id = id;
 			this.cell = cell == null ? new Cell(null, 'T') : cell;
 			this.op = op;
 			this.adaptParams = adaptParams;
 		}
-		
+
 		public void run(){
-			while(numOps.incrementAndGet() < NUM_OPERATIONS){
+			List<Character> instructions = initInstructions();
+			long startTime = System.currentTimeMillis();
+			for(char instruction : instructions){
+				this.op = instruction;
 				stackOp(this);
-				this.op = instructions.remove(0);
 			}
-			System.out.println("Finished thread " + id);
+			System.out.println("Finished thread " + id + " in " + ((System.currentTimeMillis() - startTime) / 1000.0) + " seconds");
 		}
 	}
+
+
+	//////////////////////////////////////////////////////////////
+	////////////////// VARIABLE DECLARATIONS /////////////////////
+	//////////////////////////////////////////////////////////////
+
 	
-	
-	// TODO: Figure out the correct values for ADAPT_INIT, MAX_COUNT, MAX_FACTOR, and MIN_FACTOR
-	//and figure our how the delay(spin) should work
-	private static final int NUM_THREADS = 8;
+	//final integers for number of threads to use and the number of operations
+	//for EACH thread to perform.
+	private static final int NUM_THREADS = 4;
 	private static final int NUM_OPERATIONS = 500000;
+	//data to put in the cells that are pushed to stack (not important in this case,
+	//just used to represent a type of data the stack is holding)
 	private static char currentData = 'a';
 	private static SimpleStack S = new SimpleStack(null);
-	private static AtomicInteger numOps = new AtomicInteger(0), stackSize = new AtomicInteger(0);
+	//used to generate list of instructions, randomizing push and pops
 	private static Random random = new Random();
-	private static ArrayList<Character> instructions = new ArrayList<Character>();
+	//shared lock object used in synchronized block to make stack operations thread safe
 	private static Object lock = new Object();
+
+	
+	//////////////////////////////////////////////////////////////
+	//////////////////// METHOD DECLARATIONS /////////////////////
+	//////////////////////////////////////////////////////////////
+
 	
 	public static void main(String[] args) {
 		ThreadInfo currentThread;
 		initStack();
-		initInstructions();
 		for(int i = 0; i < NUM_THREADS; i++){
-			currentThread = new ThreadInfo(i, instructions.get(i), null, null);
+			currentThread = new ThreadInfo(i, ' ', null, null);
 			currentThread.start();
 		}
-		System.out.println("Made it to the end of main()");
 	}
 
+	/**
+	 * Performs an operation on the shared SimpleStack S. Does this by entering synchronized
+	 * block on Object lock. if its a pop, add the thread's cell to the top of the stack
+	 * and point its next cell to the old top. Then a new cell must be created for the thread.
+	 * If it is a pop, we simply set the top of the stack to the old top's next cell. 
+	 * There is no need to create a new cell for p in this case. This must be in a synch block
+	 * so the threads can safely operate on the shared stack object sequentially. 
+	 * 
+	 * @param p - the current thread that is trying to perform a stack operation
+	 */
 	public static void stackOp(ThreadInfo p){
 		Cell newCell;
 		synchronized(lock){
@@ -88,20 +138,19 @@ public class Main {
 				newCell.next = S.top;
 				S.top = newCell;
 				p.cell = new Cell(null, ++currentData);
-				stackSize.incrementAndGet();
 			} else{
 				if (S.top != null) {
 					S.top = S.top.next;
-					stackSize.decrementAndGet();
 				}
 			}
-			System.out.println("stack size = " + stackSize);
 		}
-		
-		
 	}
-	
-	
+
+	/**
+	 * Adds some cells to the simple stack S so it will not be empty
+	 * before we start performing operations on it. This is optional
+	 * and can be skipped to start with an empty stack
+	 */
 	private static void initStack(){
 		Cell currentCell = new Cell(null, '0');
 		Cell nextCell;
@@ -111,18 +160,25 @@ public class Main {
 			currentCell = nextCell;
 			S.top = currentCell;
 		}
-		stackSize.set(i + 1);
 	}
-	
-	private static void initInstructions(){
-		System.out.println("Started init instructions");
+
+	/**
+	 * Creates an array list of char instructions, either '+' or '-' for 
+	 * push or pop, respectively. Each thread will call this method to get
+	 * an individual and randomly generated unique array, each the length of the desired number
+	 * of operations, in this case 500,000.
+	 * 
+	 * @return a character array of instructions
+	 */
+	private static List<Character> initInstructions(){
+		List<Character> instructions = new ArrayList<Character>();
 		for(int i = 0; i < NUM_OPERATIONS; i++){
-			if(random.nextBoolean()){
+			if(random.nextDouble() < 0.50){
 				instructions.add('+');
 			}else {
 				instructions.add('-');
 			}
 		}
-		System.out.println("Finished init instructions");
+		return instructions;
 	}
 }
