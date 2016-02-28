8ae1339674a11311c8607945cf63ec66d5b782b0
Austin Pearigen <wapmon@knights.ucf.edu>
2016-02-27 19:51:03 -0500

    Threads may be colliding now. Fixed NPE's, have to see what is happening with all the threads now, some of my print lines are being hit now.

--- src/lockFreeStack/Main.java
+++ src/lockFreeStack/Main.java
@@ -1,8 +1,8 @@
 package lockFreeStack;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
 	
@@ -48,13 +48,13 @@ public class Main {
 		}
 		
 		public void run(){
-			while(numOps < NUM_OPERATIONS){
+			ArrayList<Character> instructions = initInstructions();
+			long startTime = System.currentTimeMillis();
+			for(char instruction : instructions){
+				this.op = instruction;
 				stackOp(this);
-				this.op = instructions.get(0);
-				instructions.remove(0);
-				numOps++;
 			}
-			System.out.println("Finished thread " + id);
+			System.out.println("Finished thread " + id + " in " + (System.currentTimeMillis() - startTime));
 		}
 	}
 	
@@ -63,23 +63,23 @@ public class Main {
 	}
 	
 	// TODO: Figure out the correct values for ADAPT_INIT, MAX_COUNT, MAX_FACTOR, and MIN_FACTOR
-	//and figure our how the delay(spin) should work
-	private static final int NUM_THREADS = 4;
-	private static final int NUM_OPERATIONS = 50000, ADAPT_INIT = 1, MAX_COUNT = 5, MAX_RETRIES = 3 ;
+	//and figure collision layer is applied to threads.
+	private static final int NUM_THREADS = 8;
+	private static final int NUM_OPERATIONS = 500000, ADAPT_INIT = 1, MAX_COUNT = 5, MAX_RETRIES = 10 ;
 	private static final double MIN_FACTOR = 1.0, MAX_FACTOR = 2.0;
 	private static ThreadInfo[] location = new ThreadInfo[NUM_THREADS];
 	private static int[] collision = new int[NUM_THREADS];
 	private static SimpleStack S = new SimpleStack(null);
-	private static int numOps = 0, him = -5000;
+	private static int numOps = 0;
 	private static Random random = new Random();
-	private static ArrayList<Character> instructions = new ArrayList<Character>();
+	
 	
 	public static void main(String[] args) {
 		ThreadInfo currentThread;
 		initStack();
-		initInstructions();
 		for(int i = 0; i < NUM_THREADS; i++){
-			currentThread = new ThreadInfo(i, instructions.get(i), null, null);
+			collision[i] = -5000;
+			currentThread = new ThreadInfo(i, ' ', null, null);
 			currentThread.start();
 		}
 		System.out.println("Made it to the end of main()");
@@ -87,13 +87,13 @@ public class Main {
 
 	public static void stackOp(ThreadInfo p){
 		if(!tryPerformStackOp(p)){
+			System.out.println("Stack op failed");
 			lesOp(p);
 		}
-		p.cell = S.top.next;
 	}
 
 	private static void lesOp(ThreadInfo p) {
-		int position;
+		int position, him;
 		ThreadInfo q;
 		while(true){
 			location[p.id] = p;
@@ -106,7 +106,7 @@ public class Main {
 				q = location[him];
 				if(q != null && q.id == him && q.op != p.op){
 					if(compareAndSwap(location[p.id], p, null)){
-						if(tryCollision(p, q)){
+						if(tryCollision(p, q, him)){
 							break;
 						}
 						else{
@@ -120,16 +120,17 @@ public class Main {
 					}
 				}
 			}
-		}
-		delay(p);
-		adaptWidth(Direction.SHRINK, p);
-		if(!compareAndSwap(location[p.id], p, null)){
-			finishCollision(p);
-			return;
-		}
-		
-		if(tryPerformStackOp(p)){
-			return;
+			if(!delay(p)){
+				adaptWidth(Direction.SHRINK, p);
+			}
+			if(!compareAndSwap(location[p.id], p, null)){
+				finishCollision(p);
+				break;
+			}
+			
+			if(tryPerformStackOp(p)){
+				break;
+			}
 		}
 	}
 
@@ -160,7 +161,7 @@ public class Main {
 		
 	}
 
-	private static boolean tryCollision(ThreadInfo p, ThreadInfo q) {
+	private static boolean tryCollision(ThreadInfo p, ThreadInfo q, int him) {
 		if(p.op == '+'){
 			if(compareAndSwap(location[him],q,p)){
 				return true;
@@ -188,17 +189,30 @@ public class Main {
 		Cell ptop, pnext;
 		if(p.op == '+'){
 			ptop = S.top;
+			if(p.cell == null){
+				System.out.println("null");
+				p.cell = new Cell(null, 'p');
+			}
 			p.cell.next = ptop;
-			return compareAndSwap(S.top, ptop, p.cell);
+			if(compareAndSwap(ptop, p.cell)){
+				p.cell = new Cell(null, 'p');
+				return true;
+			}
+			else{
+				return false;
+			}
 		}
-		if(p.op == '-'){
+		else if(p.op == '-'){
 			ptop = S.top;
+			
+			//stack is empty
 			if(ptop == null){
 				p.cell = null;
 				return true;
 			}
+			
 			pnext = ptop.next;
-			if(compareAndSwap(S.top, ptop, pnext)){
+			if(compareAndSwap(ptop, pnext)){
 				p.cell = ptop;
 				return true;
 			}
@@ -211,8 +225,22 @@ public class Main {
 		return false;
 	}
 	
-	private static boolean compareAndSwap(Object V, Object A, Object B){
-		if(A.equals(V)){
+	private static boolean compareAndSwap(Cell A, Cell B) {
+//		if(A == null || B == null){
+//			System.out.println("null");
+//		}
+		if((A == null && S.top == null) || S.top.equals(A)){
+			S.top = B;
+			return true;
+		}
+		else{
+			System.out.println("compareAndSwap is false");
+			return false;
+		}
+	}
+
+	private static boolean compareAndSwap(ThreadInfo V, ThreadInfo A, ThreadInfo B){
+		if(V.equals(A)){
 			V = B;
 			return true;
 		}
@@ -222,16 +250,25 @@ public class Main {
 		}
 	}
 	
+	private static boolean compareAndSwap(int V, int A, int B){
+		if(V == A){
+			V = B;
+			return true;
+		}
+		else{
+			System.out.println("compareAndSwap is false");
+			return false;
+		}
+	}
+	
+	//TODO: Needs to use Shavit and Zemach technique for diffracting trees
 	public static boolean delay(ThreadInfo p){
 		boolean retry = true;
 		int retries = 0;
 		ThreadInfo temp = p;
 		while(retry && retries < MAX_RETRIES){
-			try {
-				Thread.sleep((long) Math.pow(2, retries));
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			long startTime = System.currentTimeMillis();
+			while(System.currentTimeMillis() - startTime < Math.pow(2, retries)){
 			}
 			if(p.equals(temp)){
 				return true;
@@ -249,13 +286,15 @@ public class Main {
 		}
 	}
 	
-	private static void initInstructions(){
-		for(int i = 0; i < 520000; i++){
+	private static ArrayList<Character> initInstructions(){
+		ArrayList<Character> instructions = new ArrayList<Character>(NUM_OPERATIONS);
+		for(int i = 0; i < NUM_OPERATIONS; i++){
 			if(random.nextBoolean()){
 				instructions.add('+');
 			}else {
 				instructions.add('-');
 			}
 		}
+		return instructions;
 	}
 }
